3304570097b6fe898ad745df552ada77594ad2f8
kardelio <kardeliogithub@outlook.com>
2023-10-06 09:54:27 +0200

    Init

---
+++ compose-spin-wheel/build.gradle.kts
@@ -0,0 +1,98 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.kardelio.compose_spin_spinwheel"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+afterEvaluate{
+    publishing {
+        publications {
+            create<MavenPublication>("compose-spin-wheel") {
+                groupId = "com.kardelio"
+                artifactId = "compose-spin-wheel"
+                version = "0.1"
+                from(components["release"])
+//                println("${buildDir}/outputs/aar/compose-spin-wheel-release.aar")
+//                artifact("${buildDir}/outputs/aar/compose-spin-wheel-release.aar")
+            }
+        }
+        repositories {
+            maven {
+                name = "GithubPackages"
+                url = uri("https://maven.pkg.github.com/kardelio/compose-spin-wheel")
+                credentials {
+                    username = System.getenv("GITHUB_SPINWHEEL_USERNAME")
+                    password = System.getenv("GITHUB_SPINWHEEL_PASSWORD")
+                }
+            }
+        }
+    }
+}
+
+
+dependencies {
+
+//    implementation(libs.core.ktx)
+//    implementation(libs.appcompat)
+//    implementation(libs.compose)
+//    implementation(libs.material)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.test.ext.junit)
+//    androidTestImplementation(libs.espresso.core)
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+}
